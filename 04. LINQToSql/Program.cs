63517f44e40dcf335585c0f3198959ab5d9afb4c
student <student@itstep1.dp.ua>
2018-12-02 09:58:57 +0200

    Upload project

---
+++ 04. LINQToSql/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.LINQToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connStr = @"Data Source=server2;Initial Catalog=ttttttttt;User ID=student;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            /*DataContext ctx = new DataContext(connStr);
+            ctx.CreateDatabase();
+            var users = ctx.GetTable<User>();
+
+            // Редактирование данных
+            //var u = users.First();
+            //u.Firstname = "It`s new firstname";
+
+            //Вставка данных
+            users.InsertOnSubmit(new User { Firstname = "Kolya", Lastname = "Nikiforov" });
+
+            //Удаление данных
+            users.DeleteOnSubmit(users.Where(user => user.Id == 1002).FirstOrDefault());
+
+            ctx.SubmitChanges();
+
+            Console.WriteLine("Success");*/
+
+            Console.Read();
+        }
+    }
+}
---
+++ 04. LINQToSql/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Linq.Mapping;
+
+namespace _04.LINQToSql
+{
+    [Table(Name = "users")]
+    public class User
+    {
+        [Column(Name = "id", IsDbGenerated = true, IsPrimaryKey = true)]
+        public int Id { get; set; }
+        [Column(Name = "firstname")]
+        public string Firstname { get; set; }
+        [Column(Name = "lastname")]
+        [StringLength(50)]
+        public string Lastname { get; set; }
+    }
+}
